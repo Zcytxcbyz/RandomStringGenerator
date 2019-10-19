89f24d45bacfd7b5234e435972ad567c0fbde625
zcytxcbyz@163.com <zcytxcbyz@163.com>
2019-10-19 11:04:31 +0800

    Update

--- RandomStringGenerator-Control/Program.cs
+++ RandomStringGenerator-Control/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
-namespace RandomStringGenerator_Control
+namespace RandomStringGenerator
 {
     class Program
     {
--- RandomStringGenerator-Control/Resources.Designer.cs
+++ RandomStringGenerator-Control/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RandomStringGenerator_Control {
+namespace RandomStringGenerator {
     using System;
     
     
